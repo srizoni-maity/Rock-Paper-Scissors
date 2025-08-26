da1889fa5945083e33d3a223eac8b3c270eed697
Srizoni Maity <Zeuh75756@gmail.com>
2025-08-26 08:27:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+print("vs Quincy:", play(player, quincy, 1000))
+print("vs Abbey:", play(player, abbey, 1000))
+print("vs Kris:", play(player, kris, 1000))
+print("vs Mrugesh:", play(player, mrugesh, 1000))
